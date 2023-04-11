d0d2f97dff565cd0b907b5afb830fe5023b114a9
Carl Waldbieser <cwaldbieser@gmail.com>
2023-04-11 16:31:58 -0400

    Added ``slack_file.py cp`` command.
    Added ``slack_file.py cp`` command.
    Copy files from the cache to the filesystem.
    Useful for examining files that can't be displayed inline.

--- slack_file.py
+++ slack_file.py
@@ -2,12 +2,13 @@
 
 import argparse
 import datetime
+import sys
 
 from dateutil.tz import tzlocal
 from rich.table import Table
 
 from slackcli.console import console
-from slackcli.filecache import get_file_info, init_filecache
+from slackcli.filecache import get_file_from_cache, get_file_info, init_filecache
 
 
 def handle_list_command(filecache, args):
@@ -30,6 +31,20 @@ def handle_list_command(filecache, args):
     console.print(table)
 
 
+def handle_copy_command(filecache, args):
+    """
+    Copy cached files to the filesystem.
+    """
+    file_id = args.file_id
+    file_path = args.filename
+    file_data = get_file_from_cache(filecache, file_id)
+    if file_data is None:
+        print(f"Could not retrieve file with ID {file_id}.", file=sys.stderr)
+        sys.exit(1)
+    with open(file_path, "wb") as f:
+        f.write(file_data.read())
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Manipulate cached Slack files.")
     parser.add_argument(
@@ -38,9 +53,16 @@ if __name__ == "__main__":
         help="Slack Workspace",
     )
     subparsers = parser.add_subparsers(help="sub-command help")
-    parser_list = subparsers.add_parser("list", help="List cached file information.")
-    # parser_list.add_argument("bar", type=int, help="bar help")
+    parser_list = subparsers.add_parser(
+        "list", aliases=["ls"], help="List cached file information."
+    )
     parser_list.set_defaults(dispatcher=handle_list_command)
+    parser_copy = subparsers.add_parser(
+        "copy", aliases=["cp"], help="Copy file from cache to filesystem."
+    )
+    parser_copy.add_argument("file_id", help="The ID of the file to copy.")
+    parser_copy.add_argument("filename", help="Copy cached file to FILENAME.")
+    parser_copy.set_defaults(dispatcher=handle_copy_command)
     args = parser.parse_args()
     with init_filecache(args.workspace) as filecache:
         args.dispatcher(filecache, args)
